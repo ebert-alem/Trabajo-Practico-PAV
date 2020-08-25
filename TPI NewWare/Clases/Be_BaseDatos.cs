6d16c5ed77bf6c719cb980e8f1d1cfdb4d72923e
Axel Wismer <mmowismer@outlook.com>
2020-08-25 18:43:31 -0300

    Backend: Opereaciones de conexion y consulta a la BD

---
+++ TPI NewWare/Clases/Be_BaseDatos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TPI_NewWare.Clases
+{
+    class Be_BaseDatos
+    {
+        SqlConnection Conexion = new SqlConnection();
+        SqlCommand Cmd = new SqlCommand();
+        
+        //Cadena con la informacion del servidor y bd a conectar
+        string Cadena_conexion = "Data Source = pavTpGrupal.mssql.somee.com; Persist Security Info = True; User ID = ebertalem_SQLLogin_1; Password = 4m8u7yha3i";
+      
+        //Abre la conexion con el servidor
+        private void Conectar()
+        {
+            Conexion.ConnectionString = Cadena_conexion;
+            Conexion.Open();
+            Cmd.Connection = Conexion;
+            Cmd.CommandType = System.Data.CommandType.Text;
+        }
+
+        //Cierra la conexion con el servidor
+        private void Desconectar()
+        {
+            Conexion.Close();
+        }
+
+        //Envia una consulta sql al servidor
+        public DataTable Consulta(string sql)
+        {
+            Conectar();
+            Cmd.CommandText = sql;
+            DataTable tabla = new DataTable();
+            tabla.Load(Cmd.ExecuteReader());
+            Desconectar();
+            return tabla;
+        }
+    }
+}
