b635909b011846addb9f9ef595bbf048843cffe8
Ebert Alemán <ferbert18@gmail.com>
2020-08-28 19:01:10 -0300

    Centrar logo inicio

--- TPI NewWare/Formularios/FrmInicio.Designer.cs
+++ TPI NewWare/Formularios/FrmInicio.Designer.cs
@@ -791,9 +791,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(53, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(95, 91);
+            this.pictureBox1.Size = new System.Drawing.Size(225, 97);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
