6de32b1148fc882375b58c6930e2b549af95faaf
Axel Wismer <mmowismer@outlook.com>
2020-10-31 12:42:27 -0300

    Form Estadistica empleado por proyecto

---
+++ TPI NewWare/Reportes/EmpleadoProyecto/FrmEstadisticaEmpleadoProyecto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPI_NewWare.Reportes.EmpleadoProyecto
+{
+    public partial class FrmEstadisticaEmpleadoProyecto : Form
+    {
+        public FrmEstadisticaEmpleadoProyecto()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmEstadisticaEmpleadoProyecto_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
